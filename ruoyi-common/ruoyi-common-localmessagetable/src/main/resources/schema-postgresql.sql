5c67493f6062c675e73e957abda129fb240e0f04
QEES <fariyajahan303@gmail.com>
2025-07-03 23:38:40 +0800

    build.ruoyi-common-localmessagetable: 更新数据库表结构
    - 删除了原有的用户表结构
    - 新增了本地事务消息表结构，包括以下字段：
      - 主键 (id)
      - 类名 (class_name)
      - 方法名 (method_name)
      - 方法参数 (method_params)
      - 方法类型 (param_types)
      - 描述 (description)
      - 事务执行状态 (status)
      - 重试次数 (retry_times)
      - 最大重试次数 (max_retry_times)
      - 错误信息 (error_message)
      - 创建时间 (created_time)
      - 更新时间 (updated_time)
      - 执行时间 (executed_time)
    -为 PostgreSQL 数据库添加了表和字段的注释

--- ruoyi-common/ruoyi-common-localmessagetable/src/main/resources/schema-postgresql.sql
+++ ruoyi-common/ruoyi-common-localmessagetable/src/main/resources/schema-postgresql.sql
@@ -1,12 +1,30 @@
-CREATE TABLE IF NOT EXISTS "user" (
-                                      id BIGSERIAL PRIMARY KEY,
-                                      user_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100),
-    create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
+CREATE TABLE IF NOT EXISTS local_message_table (
+                                     id int8 NOT NULL, -- 主键
+                                     class_name varchar(256) NOT NULL, -- 类名
+                                     method_name varchar(256) NOT NULL, -- 方法名
+                                     method_params varchar(256) NULL, -- 方法参数
+                                     param_types varchar(256) NULL, -- 方法类型
+                                     description varchar(512) NULL, -- 描述
+                                     status varchar(32) NOT NULL DEFAULT 'pending'::character varying, -- 事务执行状态
+                                     retry_times int4 NOT NULL DEFAULT 0, -- 重试次数
+                                     max_retry_times int4 NOT NULL DEFAULT 3, -- 最大重试次数
+                                     error_message varchar(512) NULL, -- 错误信息
+                                     created_time timestamp NULL,
+                                     updated_time timestamp NULL,
+                                     executed_time timestamp NULL,
+                                     CONSTRAINT local_message_table_pkey PRIMARY KEY (id)
+);
+COMMENT ON TABLE public.local_message_table IS '本地事务消息表';
 
--- PostgreSQL的注释语法
-COMMENT ON TABLE "user" IS '用户表';
-COMMENT ON COLUMN "user".user_name IS '用户名';
-COMMENT ON COLUMN "user".email IS '邮箱';
+-- Column comments
+
+COMMENT ON COLUMN public.local_message_table.id IS '主键';
+COMMENT ON COLUMN public.local_message_table.class_name IS '类名';
+COMMENT ON COLUMN public.local_message_table.method_name IS '方法名';
+COMMENT ON COLUMN public.local_message_table.method_params IS '方法参数';
+COMMENT ON COLUMN public.local_message_table.param_types IS '方法类型';
+COMMENT ON COLUMN public.local_message_table.description IS '描述';
+COMMENT ON COLUMN public.local_message_table.status IS '事务执行状态';
+COMMENT ON COLUMN public.local_message_table.retry_times IS '重试次数';
+COMMENT ON COLUMN public.local_message_table.max_retry_times IS '最大重试次数';
+COMMENT ON COLUMN public.local_message_table.error_message IS '错误信息';
--- ruoyi-common/ruoyi-common-localmessagetable/src/main/resources/schema.sql
+++ ruoyi-common/ruoyi-common-localmessagetable/src/main/resources/schema.sql
@@ -1,1 +1,16 @@
-CREATE TABLE IF NOT EXISTS user (id BIGINT AUTO_INCREMENT PRIMARY KEY, user_name VARCHAR(50) NOT NULL COMMENT '用户名', email VARCHAR(100) COMMENT '邮箱') COMMENT '用户表';
+CREATE TABLE IF NOT EXISTS `local_message_table` (
+                                       `id` bigint NOT NULL COMMENT '主键',
+                                       `class_name` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '类名',
+                                       `method_name` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '方法名',
+                                       `method_params` varchar(256) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '方法参数',
+                                       `param_types` varchar(256) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '方法类型',
+                                       `description` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '描述',
+                                       `status` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'pending' COMMENT '事务执行状态',
+                                       `retry_times` int NOT NULL DEFAULT '0' COMMENT '重试次数',
+                                       `max_retry_times` int NOT NULL DEFAULT '3' COMMENT '最大重试次数',
+                                       `error_message` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '错误信息',
+                                       `created_time` datetime DEFAULT NULL,
+                                       `updated_time` datetime DEFAULT NULL,
+                                       `executed_time` datetime DEFAULT NULL,
+                                       PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='本地事务消息表';
